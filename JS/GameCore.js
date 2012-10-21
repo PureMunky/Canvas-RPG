427cbfa4fa97bc0fc7ed4e51561a2a05fa1854b2
Phil Corbett <corbett.phil@gmail.com>
2012-10-20 21:25:42 -0400

    restructure Distance object.

--- JS/GameCore.js
+++ JS/GameCore.js
@@ -12,34 +12,34 @@ function GameCore(){
 		}
 	};
 	
-	this.Distance = {};
-	this.Distance.Between = function(o1, o2) {
-		o2 = o2 || GameObjects[0];
-		p1 = o1.getPosition();
-		p2 = o2.getPosition();
-		
-		var a, b;
-		a = Math.abs(p1.x - p2.x);
-		b = Math.abs(p1.y - p2.y);
-		
-		return Math.sqrt((a * a) + (b * b));
-	}
-	
-	this.Distance.Closest = function (o1) {
-		//TODO: add a second object to compare (e.x. Closest(item) or Closest(hostile));
-		var rtnObject = {title: 'none'};
-		var shortestDistance = 1000;
-		
-		for(var i = 0; i < GameObjects.length; i++) {
-			var thisDistance = DistanceBetween(o1, GameObjects[i]);
-			if(thisDistance < shortestDistance && o1 != GameObjects[i]) {
-				shortestDistance = thisDistance;
-				rtnObject = GameObjects[i];
+	this.Distance = {
+		Between: function(o1, o2) {
+			o2 = o2 || GameObjects[0];
+			p1 = o1.getPosition();
+			p2 = o2.getPosition();
+			
+			var a, b;
+			a = Math.abs(p1.x - p2.x);
+			b = Math.abs(p1.y - p2.y);
+			
+			return Math.sqrt((a * a) + (b * b));
+		},
+		Closest: function (o1) {
+			//TODO: add a second object to compare (e.x. Closest(item) or Closest(hostile));
+			var rtnObject = {title: 'none'};
+			var shortestDistance = 1000;
+			
+			for(var i = 0; i < GameObjects.length; i++) {
+				var thisDistance = DistanceBetween(o1, GameObjects[i]);
+				if(thisDistance < shortestDistance && o1 != GameObjects[i]) {
+					shortestDistance = thisDistance;
+					rtnObject = GameObjects[i];
+				}
 			}
+			
+			return rtnObject;
 		}
-		
-		return rtnObject;
-	}
+	};
 	
 	this.GameObjects = new Array();
 	GameObjects[0] = TG.Engines.Generate.Player('Player', TG.Engines.Generate.Sex.Male());
@@ -49,7 +49,7 @@ function GameCore(){
 	GameObjects[3] = TG.Engines.Generate.NPC('C', TG.Engines.Generate.Sex.Female());
 	GameObjects[4] = TG.Engines.Generate.NPC('D', TG.Engines.Generate.Sex.Female());
 	
-	TG.Engines.Relationships.Mate(GameObjects[2], GameObjects[3]);
+	TG.Engines.Relationships.Mate(GameObjects[2], GameObjects[3]); //test mating
 	
 	for(var i = 0; i < GameObjects.length; i++) {
 		GameObjects[i].Inventory.Equip(TG.Engines.Generate.Item());
